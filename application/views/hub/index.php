a0edd1a09702712130ca10d9720f19512f2503a1
Kelnit <kelvinmulyawan2000@gmail.com>
2026-07-10 11:45:46 +0700

    Balarama

--- application/views/hub/index.php
+++
@@ -1,8 +0,0 @@
-<!-- Panel Selamat Datang -->
-<div class="hub-page">
-  <section class="hub-greeting">
-    <p class="hub-kicker">Selamat Datang Kembali</p>
-    <h1 class="hub-name"><?= $this->session->userdata('name'); ?></h1>
-    <p class="hub-date"><?= indodate(); ?></p>
-  </section>
-</div>
